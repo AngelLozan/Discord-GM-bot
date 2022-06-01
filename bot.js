851e83e018137d87a8be327d7a261ffe81d2e1c1
Scott Lozano <lozans89@gmail.com>
2022-06-01 12:36:39 +0100

    fixes regex so character does not match middle or part of word

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ client.on('message', msg => {
 
    if (msg.author.bot){ 
       return; 
-   } else if(/good morning|^gm|mornin|morning/i.test(msg.content)){
+   } else if(/good morning|^gm$|mornin|morning/i.test(msg.content)){
       msg.reply('GM' + getRandomEmojiGM());
    } else if(/good night|nite|night/i.test(msg.content)){
       msg.reply('GN' + getRandomEmojiGN());
