26c8a90767628218eb4983be2ccf581464264470
Scott Lozano <lozans89@gmail.com>
2022-06-02 06:33:08 +0100

    bug for GM GN fix

--- bot.js
+++ bot.js
@@ -48,8 +48,12 @@ client.on('message', msg => {
       return; 
    } else if(/good morning|^gm+[^A-Za-z0-9@]|mornin|morning/i.test(msg.content)){
       msg.reply('GM' + getRandomEmojiGM());
+   } else if(msg.content.toLowerCase() === gm){
+      msg.reply('GM' + getRandomEmojiGM());
    } else if(/good night|nite|^gn+[^A-Za-z0-9@]|night/yi.test(msg.content)){
       msg.reply('GN' + getRandomEmojiGN());
+   } else if(msg.content.toLowerCase() === gn){
+      msg.reply('GM' + getRandomEmojiGN());
    }
 
 
