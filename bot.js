470284f42cd5fb1e5e1ed0bc7c922e83b478f7c4
Scott Lozano <lozans89@gmail.com>
2022-06-02 06:37:34 +0100

    test

--- bot.js
+++ bot.js
@@ -46,13 +46,13 @@ client.on('message', msg => {
 
    if (msg.author.bot){ 
       return; 
-   } else if(/good morning|^gm+[^A-Za-z0-9@]|mornin|morning/i.test(msg.content)){
+   } else if(/good morning|'^gm+[^A-Za-z0-9@]'|mornin|morning/i.test(msg.content)){
       msg.reply('GM' + getRandomEmojiGM());
-   } else if(msg.content.toLowerCase() === gm){
+   } else if(msg.content.toLowerCase() === 'gm'){
       msg.reply('GM' + getRandomEmojiGM());
-   } else if(/good night|nite|^gn+[^A-Za-z0-9@]|night/yi.test(msg.content)){
+   } else if(/good night|nite|^gn+[^A-Za-z0-9@]|night/i.test(msg.content)){
       msg.reply('GN' + getRandomEmojiGN());
-   } else if(msg.content.toLowerCase() === gn){
+   } else if(msg.content.toLowerCase() === 'gn'){
       msg.reply('GM' + getRandomEmojiGN());
    }
 
