f3f619cae0c8952156d2d9c3b8d3b4aaae40050a
Scott Lozano <lozans89@gmail.com>
2022-06-03 23:01:58 +0100

    changes emoji react order

--- bot.js
+++ bot.js
@@ -67,59 +67,59 @@ return Promise.resolve()
       return;
       msg.channel.stopTyping(); 
    } else if(/good morning|good mornin|^gm$|^gm[^A-Za-z0-9@].*$|mornin|morning/yi.test(msg.content)){
+      msg.react(getRandomEmojiGM());
       msg.channel.startTyping();
       setTimeout(()=>{
          msg.channel.send('GM ' + getRandomEmojiGM());
-         msg.react(getRandomEmojiGM());
       }, 2000);
       msg.channel.stopTyping(); 
    } else if(/\bgm\b/gi.test(msg.content)){
+      msg.react(getRandomEmojiGM());
       msg.channel.startTyping();
       setTimeout(()=>{
          msg.channel.send('GM ' + getRandomEmojiGM());
-         msg.react(getRandomEmojiGM());
       }, 2000);
       msg.channel.stopTyping(); 
    } else if(/\bmorning\b/gi.test(msg.content)){
+      msg.react(getRandomEmojiGM());
       msg.channel.startTyping();
       setTimeout(()=>{
          msg.channel.send('GM ' + getRandomEmojiGM());
-         msg.react(getRandomEmojiGM());
       }, 2000);
       msg.channel.stopTyping(); 
    } else if(/\bmornin\b/gi.test(msg.content)){
+      msg.react(getRandomEmojiGM());
       msg.channel.startTyping();
       setTimeout(()=>{
          msg.channel.send('GM ' + getRandomEmojiGM());
-         msg.react(getRandomEmojiGM());
       }, 2000);
       msg.channel.stopTyping(); 
    } else if(/good night|nite|^gn$|^gn[^A-Za-z0-9@].*$|night/yi.test(msg.content)){
+      msg.react(getRandomEmojiGN());
       msg.channel.startTyping();
       setTimeout(()=>{
          msg.channel.send('GN ' + getRandomEmojiGN());
-         msg.react(getRandomEmojiGN());
       }, 2000);
       msg.channel.stopTyping(); 
    } else if(/\bgn\b/gi.test(msg.content)){
+       msg.react(getRandomEmojiGN());
        msg.channel.startTyping();
       setTimeout(()=>{
          msg.channel.send('GN ' + getRandomEmojiGN());
-         msg.react(getRandomEmojiGN());
       }, 2000);
       msg.channel.stopTyping(); 
    } else if(/\bnight\b/gi.test(msg.content)){
+       msg.react(getRandomEmojiGN());
        msg.channel.startTyping();
       setTimeout(()=>{
          msg.channel.send('GN ' + getRandomEmojiGN());
-         msg.react(getRandomEmojiGN());
       }, 2000);
       msg.channel.stopTyping(); 
    } else if(/\bnite\b/gi.test(msg.content)){
+       msg.react(getRandomEmojiGN());
        msg.channel.startTyping();
       setTimeout(()=>{
          msg.channel.send('GN ' + getRandomEmojiGN());
-         msg.react(getRandomEmojiGN());
       }, 2000);
       msg.channel.stopTyping(); 
    } 
@@ -131,10 +131,10 @@ return Promise.resolve()
       return;
       msg.channel.stopTyping(); 
    } else if(/good afternoon|afternoon/yi.test(msg.content)){
+      msg.react(getRandomEmojiGM());
       msg.channel.startTyping();
       setTimeout(()=>{
          msg.channel.send('Good Afternoon ' + getRandomEmojiGM());
-         msg.react(getRandomEmojiGM());
       }, 2000);
       msg.channel.stopTyping(); 
    }
