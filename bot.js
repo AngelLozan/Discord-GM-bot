19a87ed15629c470f8edb243b65f31b31296b42a
Scott Lozano <lozans89@gmail.com>
2022-06-03 23:22:19 +0100

    removes one version of afternoon

--- bot.js
+++ bot.js
@@ -100,7 +100,7 @@ return Promise.resolve()
          msg.react(getRandomEmojiGM());
       }, 2000);
       msg.channel.stopTyping(); 
-   } else if(/good afternoon|afternoon/yi.test(msg.content)){
+   } else if(/^.*\bgood afternoon\b.*$/gi.test(msg.content)){
       msg.channel.startTyping();
       setTimeout(()=>{
          msg.channel.send('Good Afternoon ' + getRandomEmojiGM());
