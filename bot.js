238bf21de5ce54d8ea4a46f7f2bfda1a8bb5f2a7
Scott Lozano <lozans89@gmail.com>
2022-06-03 15:15:31 +0100

    bot

--- bot.js
+++ bot.js
@@ -155,7 +155,7 @@ return Promise.resolve()
    } else if(/\bhelp\b/gi.test(msg.content)){
        msg.channel.startTyping();
       setTimeout(()=>{
-         msg.channel.send('Let me get someone who can help: <@841402856497610772>');
+         msg.channel.send('Let me get someone who can help: <@980467385398079488>');
          msg.react('🛟');
       }, 1000);
       msg.channel.stopTyping();  
