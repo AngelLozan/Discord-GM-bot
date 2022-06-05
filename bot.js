323b271d6bd165543d3d36695bdae61befd1c402
Scott Lozano <lozans89@gmail.com>
2022-06-05 06:48:58 +0100

    bot

--- bot.js
+++ bot.js
@@ -110,7 +110,7 @@ return Promise.resolve()
          msg.react(getRandomEmojiGM());
       }, 2000);
       msg.channel.stopTyping(); 
-   } else if(/good night|goodnight|nite nite|night night|*nite$|^gn$|^gn[^A-Za-z0-9@].*$|^night$/gi.test(msg.content)){
+   } else if(/good night|goodnight|*nite$|^gn$|^gn[^A-Za-z0-9@].*$|^night$/yi.test(msg.content)){
       msg.channel.startTyping();
       setTimeout(()=>{
          msg.channel.send('GN ' + getRandomEmojiGN());
