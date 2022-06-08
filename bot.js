868a59e3c431023339a7f1e8672211eedb2dc85c
Scott Lozano <lozans89@gmail.com>
2022-06-08 11:20:39 +0100

    adds keyword don't to list of non respond words

--- bot.js
+++ bot.js
@@ -73,7 +73,7 @@ client.on('message', msg => {
         .then(() => {
             if (msg.author.bot) {
                 return;
-            } else if (/^gm bot$|^no$|bad|didn\'t|^not$|couldn\'t|wouldn\'t|horrible|awful|terrible/gi.test(msg.content)) {
+            } else if (/^gm bot$|^no$|bad|don\'t|didn\'t|^not$|couldn\'t|wouldn\'t|horrible|awful|terrible/gi.test(msg.content)) {
                 return;
             } else if (/good morning|good mornin|^gm$|^gm[^A-Za-z0-9@].*$|^mornin$|^morning$/yi.test(msg.content)) {
                 if (talkedRecentlyGM.has(msg.author.id)) {
@@ -296,7 +296,7 @@ client.on('message', msg => {
                     msg.channel.send('I\'m not programmed to answer questions here, but the support team is always available and will respond quickly. You can email them at example.email.com if you don\'t hear from someone here. Ping the @moderation team too or find us on Twitter. You can also check out our knowledge base at https://example.com/ for helpful tutorials.');
                 }, 2000);
                 msg.channel.stopTyping();
-            } else if (/gm bot|^no$|bad|didn\'t|^not$|couldn\'t|wouldn\'t|horrible|awful|terrible/gi.test(msg.content)) {
+            } else if (/gm bot|^no$|bad|didn\'t|don't|^not$|couldn\'t|wouldn\'t|horrible|awful|terrible/gi.test(msg.content)) {
                 return;
             } else if (msg.content.toLowerCase().includes('thanks') && msg.content.includes('<@980467385398079488>')) {
                 msg.react('💙');
