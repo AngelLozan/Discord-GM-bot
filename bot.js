240ac7202e0fe59ecc2af799d1625a020f17ec6a
Scott Lozano <lozans89@gmail.com>
2022-06-08 11:42:05 +0100

    removes emoji not supported by discordjs

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.login(process.env.DISCORD_TOKEN);
 
 const getRandomEmojiGM = () => {
 
-    const emojis = ['😀', '😃', '😄', '😁', '✌🏼', '💯', '🦾', '🫡', '💙', '😎', '🚀', '😉', '☀️', '😊', '😇', '🤙', '🖖', '👆', '👋', '👾', '🌤', '🌈', '🌞', '⛅️', '✨', '💫', '🌅', '🌇', '🌄', '💎']
+    const emojis = ['😀', '😃', '😄', '😁', '✌🏼', '💯', '🦾', '🫡', '💙', '😎', '🚀', '😉', '☀️', '😊', '😇', '🤙', '🖖', '👆', '👋', '👾', '🌤', '🌈', '🌞', '✨', '💫', '🌅', '🌇', '🌄', '💎']
 
     return emojis[~~(Math.random() * emojis.length)]
 
@@ -71,8 +71,9 @@ client.on('message', msg => {
         .then(() => {
             if (msg.author.bot) {
                 return;
-            } else if (/^gm bot$|^no$|bad|didn\'t|don\'t|^not$|couldn\'t|wouldn\'t|horrible|awful|terrible/gi.test(msg.content)) {
+            } else if (/gm bot|^no$|bad|didn\'t|don\'t|not|couldn\'t|wouldn\'t|horrible|awful|terrible/gi.test(msg.content)) {
                 return;
+                msg.channel.stopTyping();
             } else if (/good morning|good mornin|^gm$|^gm[^A-Za-z0-9@].*$|^mornin$|^morning$/yi.test(msg.content)) {
                 if (talkedRecentlyGM.has(msg.author.id)) {
                     msg.channel.startTyping();
@@ -281,7 +282,12 @@ client.on('message', msg => {
         //GM bot and minor support messages
 
         .then(() => {
-            if (msg.content.toLowerCase().includes('help') && msg.content.includes('<@980457022971600936>')) {
+            if (msg.author.bot) {
+                return;
+            } else if (/gm bot|^no$|bad|didn\'t|don\'t|not|couldn\'t|wouldn\'t|horrible|awful|terrible/gi.test(msg.content)) {
+                return;
+                msg.channel.stopTyping();
+            } else if (msg.content.toLowerCase().includes('help') && msg.content.includes('<@980457022971600936>')) {
                 msg.react('⛑');
                 msg.channel.startTyping();
                 setTimeout(() => {
@@ -295,9 +301,7 @@ client.on('message', msg => {
                     msg.channel.send('I\'m not programmed to answer questions here, but the support team is always available and will respond quickly. You can email them at example.email.com if you don\'t hear from someone here. Ping the @moderation team too or find us on Twitter. Check out our knowledge base at https://support.exodus.com/ for helpful guides');
                 }, 2000);
                 msg.channel.stopTyping();
-            } else if (/gm bot|^no$|bad|didn\'t|don\'t|^not$|couldn\'t|wouldn\'t|horrible|awful|terrible/gi.test(msg.content)) {
-                return;
-            } else if (msg.content.toLowerCase().includes('thanks') && msg.content.includes('<@980457022971600936>')) {
+            }  else if (msg.content.toLowerCase().includes('thanks') && msg.content.includes('<@980457022971600936>')) {
                 msg.react('💙');
                 msg.channel.startTyping();
                 setTimeout(() => {
