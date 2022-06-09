bad88ef6326f1141dd23284c439f42b004f1f1e1
Scott Lozano <lozans89@gmail.com>
2022-06-09 13:57:39 +0100

    bot

--- bot.js
+++ bot.js
@@ -290,7 +290,7 @@ client.on('message', msg => {
                }
             })
 
-            msg.react(emojiToReactWith);
+            msg.react('emojiToReactWith');
 
             // if (/<@841402856497610772>/i.test(msg.content)) {
             //     msg.react('🚀');
