ea53fef405dde89836e824567d3d0f309232a438
Scott Lozano <lozans89@gmail.com>
2022-06-10 20:44:52 +0100

    bot

--- bot.js
+++ bot.js
@@ -79,6 +79,16 @@ client.on('message', msg => {
                 msg.channel.stopTyping();
             } else if (/gm bot|\bno\b|bad|bot|don\'t|didn\'t|not|couldn\'t|wouldn\'t|horrible|awful|terrible/gi.test(msg.content)) {
                 return;
+            } else if (botCoolDownSet.has(msg.author.bot)){
+               msg.channel.startTyping();
+                    setTimeout(() => {
+                        msg.channel.send('brb in 45 seconds, bot cool down')
+                            .then(msg => {
+                                msg.delete({ timeout: 3000 })
+                            })
+                    }, 2000)
+                    msg.channel.stopTyping();
+               return;
             } else if (/good morning|good mornin|^gm$|^gm[^A-Za-z0-9@].*$|^mornin$|^morning$/yi.test(msg.content)) {
                 if (botCoolDownSet.has(msg.author.bot)){
                   return;
