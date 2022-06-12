f3ad53e4134a185acb3a53902b4e6d61028d5139
Scott Lozano <lozans89@gmail.com>
2022-06-12 10:17:32 +0100

    bot

--- bot.js
+++ bot.js
@@ -273,7 +273,7 @@ client.on('message', msg => {
         function containsAny(str, substrings) {
             for (var i = 0; i != substrings.length; i++) {
                 var substring = substrings[i];
-                if (str.indexOf(substring != -1) {
+                if (str.indexOf(substring) != -1) {
                     if(substring === 'smile'){
                      return '😀';
                     } else if (substring === 'frown'){
